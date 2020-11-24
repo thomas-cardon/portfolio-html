4c40287a19a39dc1ee5e91462ea0a47d39898da7
Thomas Cardon <thomas.cardon@pm.me>
2020-11-24 04:07:51 +0100

    Update themeChanger.js

--- assets/js/themeChanger.js
+++ assets/js/themeChanger.js
@@ -1,12 +1,12 @@
 function changeCSS(cssFile, cssLinkIndex) {
-    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+    var oldlink = document.getElementsByTagName('link').item(cssLinkIndex);
+    var newlink = document.createElement('link');
 
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", cssFile);
+    newlink.setAttribute('rel', 'stylesheet');
+    newlink.setAttribute('type', 'text/css');
+    newlink.setAttribute('href', cssFile);
 
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    document.getElementsByTagName('head').item(0).replaceChild(newlink, oldlink);
 }
 
 const theme = localStorage.getItem('theme');
