bd64a6c6e616c6eeba574bd9b6fd03180cee8004
Thomas Cardon <thomas.cardon@pm.me>
2020-12-04 11:21:04 +0100

    CSS update

--- assets/js/themeChanger.js
+++ assets/js/themeChanger.js
@@ -1,21 +1,26 @@
-function changeCSS(cssFile, cssLinkIndex) {
-    var oldlink = document.getElementsByTagName('link').item(cssLinkIndex);
-    var newlink = document.createElement('link');
+const Theme = {
+  changeCSS: function(cssFile, cssLinkIndex) {
+      var oldlink = document.getElementsByTagName('link').item(cssLinkIndex);
+      var newlink = document.createElement('link');
 
-    newlink.setAttribute('rel', 'stylesheet');
-    newlink.setAttribute('type', 'text/css');
-    newlink.setAttribute('href', cssFile);
+      newlink.setAttribute('rel', 'stylesheet');
+      newlink.setAttribute('type', 'text/css');
+      newlink.setAttribute('href', cssFile);
 
-    document.getElementsByTagName('head').item(0).replaceChild(newlink, oldlink);
-}
+      document.getElementsByTagName('head').item(0).replaceChild(newlink, oldlink);
+  },
+  load: function() {
+    const theme = localStorage.getItem('theme');
 
-const theme = localStorage.getItem('theme');
+    if(!theme) {
+      localStorage.setItem('theme', 'light');
+    }
+    else {
+      console.log('Loading theme:', theme);
+    }
 
-if(!theme) {
-  localStorage.setItem('theme', 'light');
-}
-else {
-  console.log('Loading theme:', theme);
+
+  }
 }
 
 /*
